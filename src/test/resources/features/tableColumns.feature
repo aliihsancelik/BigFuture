cc895a1336971a5d69a5c1b1b66a67b9812e84ba
Ali Celik <aliihsancelik1@gmail.com>
2020-10-28 00:15:30 +0000

    updated

--- src/test/resources/features/tableColumns.feature
+++ src/test/resources/features/tableColumns.feature
@@ -4,10 +4,11 @@ Feature: table columns
   Scenario: verify table columns
     Given the user login as a "student"
     Then the user should see the following columns
-      | Actions  |
-      | ISBN     |
-      | Name     |
-      | Author   |
-      | Category |
-      | Year     |
+      | Actions     |
+      | ISBN        |
+      | Name        |
+      | Author      |
+      | Category    |
+      | Year        |
+      | Borrowed By |
 
--- target/default-html-reports/report.js
+++ target/default-html-reports/report.js
@@ -15,7 +15,7 @@ formatter.scenario({
   ]
 });
 formatter.before({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "the user login as a \"student\"",
@@ -25,7 +25,7 @@ formatter.match({
   "location": "com.libraryCT.step_definitions.modulesStepDefs.the_user_login_as_a(java.lang.String)"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "the user should see the following columns",
@@ -39,11 +39,15 @@ formatter.step({
   ],
   "keyword": "Then "
 });
-formatter.match({});
-formatter.result({
-  "status": "undefined"
+formatter.match({
+  "location": "com.libraryCT.step_definitions.tableColumnsStepDefs.the_user_should_see_the_following_columns(java.util.List\u003cjava.lang.String\u003e)"
 });
+formatter.result({
+  "error_message": "java.lang.AssertionError: expected:\u003c[Actions, ISBN, Name, Author, Category, Year]\u003e but was:\u003c[Actions, ISBN, Name, Author, Category, Year, Borrowed By]\u003e\n\tat org.junit.Assert.fail(Assert.java:89)\n\tat org.junit.Assert.failNotEquals(Assert.java:835)\n\tat org.junit.Assert.assertEquals(Assert.java:120)\n\tat org.junit.Assert.assertEquals(Assert.java:146)\n\tat com.libraryCT.step_definitions.tableColumnsStepDefs.the_user_should_see_the_following_columns(tableColumnsStepDefs.java:18)\n\tat ✽.the user should see the following columns(file:///Users/aliihsancelik/IdeaProjects/BigFuture/src/test/resources/features/tableColumns.feature:6)\n",
+  "status": "failed"
+});
+formatter.embedding("image/png", "embedded0.png", "screenshot");
 formatter.after({
-  "status": "skipped"
+  "status": "passed"
 });
 });
