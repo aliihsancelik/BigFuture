8102af23a218c3565d530d94027cc49e9aa52c5d
Ali Celik <aliihsancelik1@gmail.com>
2020-10-28 00:42:59 +0000

    updated

--- target/default-html-reports/report.js
+++ target/default-html-reports/report.js
@@ -15,7 +15,7 @@ formatter.scenario({
   ]
 });
 formatter.before({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "the user login as a \"student\"",
@@ -25,17 +25,19 @@ formatter.match({
   "location": "com.libraryCT.step_definitions.modulesStepDefs.the_user_login_as_a(java.lang.String)"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "the user should see all options,choose any options and search a book",
   "keyword": "Then "
 });
-formatter.match({});
+formatter.match({
+  "location": "com.libraryCT.step_definitions.categoriesStepDefs.the_user_should_see_all_options_choose_any_options_and_search_a_book()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
 });
 formatter.after({
-  "status": "skipped"
+  "status": "passed"
 });
 });
